cf144cf280a0e661216259af9186a61250a41bc4
Yena <pyo.yena.de@gmail.com>
2025-09-22 16:54:15 +0200

    first try

---
+++ project1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# def sine
+
+# def square
+
+# def triangular
+
+# def sawtooth
+
+# def unit_step
+
+# def unit_impulse
+
+# plot them (e.g. time shifting, time scaling, addition, multiplication)
+# (optional) Compute and plot the Fourier series and Fourier transform of signals
