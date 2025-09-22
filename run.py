f93a29acab3418e40252423a8af9d09007cab89e
Yena <pyo.yena.de@gmail.com>
2025-09-22 16:53:35 +0200

    final

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from signals import generate_sine_wave
 import matplotlib.pyplot as plt
 
-wave = generate_sine_wave(5, 10, 100)
+frequency = 5
+duration = 10
+sample_rate=100
+
+wave = generate_sine_wave(frequency, duration, sample_rate)
 
 t = [i / sample_rate 
      for i in range(int(duration * sample_rate))]
