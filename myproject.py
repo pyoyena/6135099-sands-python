352ef04bcf3994b944eb88251366513fc874c9d4
Yena <pyo.yena.de@gmail.com>
2025-09-29 11:20:53 +0200

    sine and unit step functions

---
+++ myproject.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(-2*np.pi, 2*np.pi, 1000)
+
+def generate_sine(f, A):
+    return A*np.sin(2*np.pi*f*t)
+
+def generate_unit_step(t0, A):
+    return np.where(t>=t0, A, 0)
+
+# def generate_square():
+
+# def square
+
+# def triangular
+
+# def sawtooth
+
+# def unit_impulse
+
+
+# TESTING OUT THE FUNCTIONS
+
+plt.plot(t, generate_sine(1/(2*np.pi), -2))
+plt.title("Sine Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
+
+plt.plot(t, generate_unit_step(1, 2))
+plt.title("Unit Step Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
+        
+
+# OPERATIONS ON FUNCTIONS
+
+# perform operationson signald and plot them (time shifcting, tie scafling, addition, multiplication)
+# (optional) Compute and plot the Fourier series and Fourier transform of signals
