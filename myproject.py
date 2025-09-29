21bcf36b3a5b2d1033a1d76f79bed77b3699ff9f
Yena <pyo.yena.de@gmail.com>
2025-09-29 11:34:41 +0200

    unit impulse function but it doesn't show on the graph

--- myproject.py
+++ myproject.py
@@ -6,36 +6,40 @@ t = np.linspace(-2*np.pi, 2*np.pi, 1000)
 def generate_sine(f, A):
     return A*np.sin(2*np.pi*f*t)
 
-def generate_unit_step(t0, A):
-    return np.where(t>=t0, A, 0)
-
 # def generate_square():
-
-# def square
 
 # def triangular
 
 # def sawtooth
+def generate_unit_step(t0, A):
+    return np.where(t>=t0, A, 0)
 
-# def unit_impulse
+def generate_unit_impulse(t0):
+    return np.where(t==t0, 1, 0)
 
 
 # TESTING OUT THE FUNCTIONS
 
-plt.plot(t, generate_sine(1/(2*np.pi), -2))
+plt.plot(t, generate_sine(1/(2*np.pi), -1))
 plt.title("Sine Signal")
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.grid(True)
 plt.show()
 
-plt.plot(t, generate_unit_step(1, 2))
+plt.plot(t, generate_unit_step(1, 1))
 plt.title("Unit Step Signal")
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.grid(True)
 plt.show()
         
+plt.plot(t, generate_unit_impulse(0))
+plt.title("Unit Impulse Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
 
 # OPERATIONS ON FUNCTIONS
 
